4af7addbbf92fc89d87c32cfc2b882069af21d05
AJ Meireles <ajmeireles>
2021-09-10 21:54:47 -0300

    General Improvements

--- README.md
+++ README.md
@@ -1,3 +1,52 @@
-# One Line Validation, CodeIgniter 4
+# One Line Validation, For CodeIgniter 4
 
-...
+## What Is?
+**One Line Validation (OLV)** is a package made for CodeIgniter 4 to provide a fast and single line validation experience ideal for CDN and/or API services consuming the Validation System from CodeIgniter 4.
+
+## When Use It?
+You should use it when you want to perform validations (with CodeIngiter 4's Validation Service) without writing too many lines of code to instanciate the Validation Service, rules and run the validation, see the examples below.
+
+## Examples
+```php
+use AJMeireles\OLV\OneLineValidation;
+
+// passing rules directly as array
+
+$validator = (new OneLineValidation)->rules([
+    'username' => [
+        'label' => 'Username', 
+        'rules' => 'alpha|min_length[20]'
+    ],
+])->run(['username' => 'Anthony']);
+```
+
+```php
+use AJMeireles\OLV\OneLineValidation;
+
+// passing rules as a variable from app\Config\Validation.php
+
+$validator = (new OneLineValidation)->rules('register')->run([
+    'username' => 'Anthony'
+]);
+
+// in this case, the $register public variable need exist in app\Config\Validation.php:
+
+public $register = [
+    'username' => [
+        'label' => 'Username', 
+        'rules' => 'alpha|exact_length[4]'
+    ]
+];
+```
+
+## Tips:
+
+### Returning All Errors
+- You can pass ```true``` for the second parameter of the ```run()``` method so that all errors will be returned, instead of stopping validation on each error.
+
+### Validate the Request
+- **If you don't define the first argument of the ```run()``` method as an array of data to be validated**, the system will use the request to perform the validation.
+
+
+## Contribution
+- Please feel invited to send a PR if you notice that there is an improvement to be added or a correction to be made.
--- src/OneLineValidation.php
+++ src/OneLineValidation.php
@@ -3,13 +3,25 @@
 namespace AJMeireles\OLV;
 
 use Config\Services;
-use Config\Validation;
+use CodeIgniter\HTTP\IncomingRequest;
+use CodeIgniter\Validation\Validation;
+use Config\Validation as ValidationConfig;
 use CodeIgniter\Validation\Exceptions\ValidationException;
-use AJMeireles\OLV\Exceptions;
 
 class OneLineValidation
 {
+	/**
+	 * Validation service
+	 *
+	 * @var Validation
+	 */
 	protected $validation = null;
+
+	/**
+	 * Request service
+	 *
+	 * @var IncomingRequest
+	 */
 	protected $request    = null;
 
 	public function __construct()
@@ -18,26 +30,41 @@ class OneLineValidation
 		$this->request    = Services::request();
 	}
 
-	public function rules($type): OneLineValidation
+	/**
+	 * Set the rules to be used
+	 *
+	 * @param array|string $rule The rule to be used
+	 * 
+	 * @return OneLineValidation
+	 */
+	public function rules($rule): OneLineValidation
 	{
-		if (is_array($type)) {
-			$this->validation->setRules($type);
+		if (is_array($rule) === true) {
+			$this->validation->setRules($rule);
 		}
 
-		if (is_string($type)) {
-			$type = strtolower($type);
-			$type = trim($type);
+		if (is_string($rule) === true) {
+			$rule = strtolower($rule);
+			$rule = trim($rule);
 
-			if (!isset((new Validation)->$type)) {
-				throw new ValidationException::forRuleNotFound('Validation: The specified rule was not defined at \Config\Validation: ' . $type);
+			if (!isset((new ValidationConfig)->$rule)) {
+				throw new ValidationException(lang('Validation.ruleNotFound', [$rule]));
 			}
 
-			$this->validation->setRuleGroup($type);
+			$this->validation->setRuleGroup($rule);
 		}
 
 		return $this;
 	}
 
+	/**
+	 * Run the validation
+	 *
+	 * @param mixed $data Optional to set data to be validated
+	 * @param boolean $allErrors If will return all errors
+	 * 
+	 * @return mixed
+	 */
 	public function run($data = null, bool $allErrors = false)
 	{
 		if (!$this->validation->withRequest($this->request)->run($data)) {
@@ -47,6 +74,7 @@ class OneLineValidation
 				return $errors;
 			}
 
+			// if $allErrors is false, we will stop in each error
 			foreach ($errors as $error) {
 				return $error;
 			}
